f19e8af3fb34cc9ae58420505f329ceb30020a9d
ritesh0403 <riteshpatil0403@gmail.com>
2025-03-01 01:23:57 +0530

    second Solution

--- Strings/ValidParentheses.java
+++ Strings/ValidParentheses.java
@@ -3,7 +3,7 @@ import java.util.*;
 //leetcode 20. Valid Parentheses
 public class ValidParentheses {
     public static void main(String[] args) {
-        Solution obj = new Solution();
+        Solution1 obj = new Solution1();
         System.out.println("()");
         System.out.println(obj.isValid("()"));
         System.out.println("()[]{}");
@@ -14,7 +14,7 @@ public class ValidParentheses {
         System.out.println(obj.isValid("([])"));
     }
 }
-class Solution {
+class Solution1 {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
@@ -48,5 +48,28 @@ class Solution {
 
         }
         return stack.isEmpty() ? true : false;
+    }
+}
+
+//using hashmap and stack
+class Solution2 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> mapping = new HashMap<>();
+        mapping.put(')', '(');
+        mapping.put('}', '{');
+        mapping.put(']', '[');
+
+        for (char c : s.toCharArray()) {
+            if (mapping.containsValue(c)) {
+                stack.push(c);
+            } else if (mapping.containsKey(c)) {
+                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();        
     }
 }
